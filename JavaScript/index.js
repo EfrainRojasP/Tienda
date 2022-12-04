1960c9795ed216a6ac2145e75739c4a19739f1a4
EfrainRojas <efrainulisesrojaspina@gmail.com>
2022-12-04 00:12:36 -0600

    Almacenamiento de comentarios

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -3,6 +3,23 @@ const txtAreaComentario = document.getElementById("cajaComentaio");
 const btnComentar = document.getElementById("comentar");
 const divComentarioUsuario = document.getElementById("comentarios");
 
+//Inicializamos el array
+window.localStorage.setItem("comentarios", JSON.stringify([]));
+
+class Comentario {
+    nombreUsuario;
+    comentario;
+    /**
+     * Constructor
+     * @param {String} nombreUsuario Nombre del usuario
+     * @param {String} comentario Comentario
+     */
+    constructor(nombreUsuario, comentario) {
+        this.nombreUsuario = nombreUsuario;
+        this.comentario = comentario;
+    }
+};
+
 //btnComentar.disabled = true;
 btnComentar.addEventListener("click", () =>{
     if(nombreUsuario.value.length === 0){
@@ -14,8 +31,12 @@ btnComentar.addEventListener("click", () =>{
         return
     }
     comentar();
+    guardarComentarios();
 });
 
+/**
+ * Aparecer los comentarios
+ */
 function comentar() {
     const parrafoNomUsuario = document.createElement("p");
     const comentarioUsuario = document.createElement("p");
@@ -25,4 +46,23 @@ function comentar() {
     divComentarioUsuario.appendChild(parrafoNomUsuario);
     divComentarioUsuario.appendChild(comentarioUsuario);
     divComentarioUsuario.appendChild(separador);
+}
+
+/**
+ * Almacena los comentario en el localstorage
+ */
+function guardarComentarios() {
+    const objComentario = new Comentario(nombreUsuario.value, txtAreaComentario.value);
+    let arrComantarios = recuperarComentatios();
+    arrComantarios.push(objComentario);
+    window.localStorage.setItem("comentarios", JSON.stringify(arrComantarios));
+}
+
+/**
+ * Recupera los comantarios del localstorage
+ * @returns Devuelve un arreglo con los comentarios
+ */
+function recuperarComentatios() {
+    let arrComantarios = JSON.parse(window.localStorage.getItem("comentarios"));
+    return arrComantarios;
 }
